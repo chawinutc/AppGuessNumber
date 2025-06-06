833c0ab767e48e040b9307efd760758f66226f3b
chawinutc <chawin@utc.ac.th>
2025-06-06 14:07:44 +0700

    success commit

--- src/appguessnumber/AppGuessNumber.java
+++ src/appguessnumber/AppGuessNumber.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package appguessnumber;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  *
  * @author Zenbook
@@ -15,6 +16,31 @@ public class AppGuessNumber {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int numRandom;
+        int numGuess;
+        int count = 0;
+        Random ran = new Random();
+        Scanner scan = new Scanner(System.in);
+        numRandom = ran.nextInt(10);
+        //System.out.println("Number is "+ numRandom);
+        while(count<=3){        
+            System.out.print("Enter your guess : ");
+            numGuess = scan.nextInt();
+            //System.out.println("Compair : "+ (numRandom==numGuess));
+            if(numGuess==numRandom){
+                System.out.println("You Win !");
+                break;
+            }else if(numGuess>numRandom){
+                System.out.println("<<<< decrement");
+                count++;
+            }else if (numGuess<numRandom){
+                System.out.println("Increment >>>>>");
+                count++;
+            }
+        }
+            if(count>=3){
+                System.out.println("You loose !!"); 
+            }       
     }
     
 }
